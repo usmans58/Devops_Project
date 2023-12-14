60c524f9a803471d28e0614e757ce0b5e8159cc6
Usman Siddiqui <usmansiddiqui51@gmail.com>
2023-12-14 20:00:02 +0500

    added Doctor

--- doctors/app.py
+++ doctors/app.py
@@ -6,8 +6,8 @@ doctors = [
   { 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  },
   { 'id': "3",'firstName': "Usman", 'lastName': "Siddiqui",'speciality':"Pediatrics"  },
   { 'id': "4",'firstName': "Umar", 'lastName': "Farooq",'speciality':"Surgeon"  },
-  { 'id': "5",'firstName': "Shoaib", 'lastName': "Ali",'speciality':"Neurology"  }
-#  { 'id': "6",'firstName': "Ahmed", 'lastName': "Ali",'speciality':"Programming"  }
+  { 'id': "5",'firstName': "Shoaib", 'lastName': "Ali",'speciality':"Neurology"  },
+  { 'id': "6",'firstName': "Ahmed", 'lastName': "Ali",'speciality':"Programming"  }
 
 
 ]
