1a53fec58a5d4dab6dd95fc3928867f52a392af4
Usman Siddiqui <usmansiddiqui51@gmail.com>
2023-12-14 22:31:28 +0500

    Added Appointments

--- appointments/app.py
+++ appointments/app.py
@@ -6,7 +6,23 @@ appointments = [
   { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
   { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
   { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  # { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "6",'doctor': "3", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "7",'doctor': "4", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "8",'doctor': "5", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "9",'doctor': "6", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "10",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "11",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "12",'doctor': "3", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "13",'doctor': "4", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "14",'doctor': "5", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "15",'doctor': "6", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "16",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "17",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "18",'doctor': "3", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "19",'doctor': "4", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "20",'doctor': "5", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "21",'doctor': "6", 'date': "22 Nov 2023", 'rating':"Good"  }
 ]
 
 @app.route('/hello')
